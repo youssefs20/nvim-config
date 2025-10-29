ef2ed1d94ae269afd59fc6a6a7858a4be727e998
Youssef Sameh <yousef17453@gmail.com>
2025-10-29 19:49:49 +0300

    added telescope , live grep and treesitter

--- init.lua
+++ init.lua
@@ -2,6 +2,9 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+-- press space + f + g to live grep
+vim.g.mapleader = " " 
+
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -32,11 +35,29 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {"nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false, build = ":TSUpdate"}
 }
 local opts = {}
-
+ 
 require("lazy").setup(plugins , opts)
+
+-- press ctrl + p to navigate bewteen files
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+local config = require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+  ensure_installed = { "c", "lua","query", "markdown", "markdown_inline", "javascript"},
+  highlight = { enable = true},
+  indent = {enable = true},
+
+}
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
